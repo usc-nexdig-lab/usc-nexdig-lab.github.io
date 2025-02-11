da9535ab0a115bfd139a0823010dec87efa916ec
LightningNemesis <ak.kumar220919@gmail.com>
2025-02-11 12:59:20 -0800

    Updated SVG logo on /home and header | Updated Prof's image in /people | Fixed refresh redirection to / without hashRouter

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDigging, faFistRaised, faPeopleGroup, faNewspaper } from "@fortawesome/free-solid-svg-icons";
-import { Link, useLocation } from "react-router-dom";
-import NEXDIG_LOGO from "assets/logo.svg";
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faDigging,
+  faFistRaised,
+  faPeopleGroup,
+  faNewspaper,
+} from '@fortawesome/free-solid-svg-icons';
+import { Link, useLocation } from 'react-router-dom';
+import NEXDIG_LOGO from 'assets/logo_updated.svg';
 export const Header = () => {
   const location = useLocation();
   const [clickedIcon, setClickedIcon] = useState<string | null>(null);
@@ -16,43 +21,124 @@ export const Header = () => {
 
   return (
     <div className="fixed bg-white h-[60px] shadow w-full flex items-center space-x-[30px] px-[30px] py-[10px] text-sm tracking-wide z-50 text-gray-800 font-medium">
-     <div className="flex-1">
+      <div className="flex-1">
         <Link to="/">
-          <img src={NEXDIG_LOGO} alt="nexdig" className="h-[20px] cursor-pointer" />
+          <img
+            src={NEXDIG_LOGO}
+            alt="nexdig"
+            className="h-[20px] cursor-pointer"
+          />
         </Link>
       </div>
 
-      <Link to="/" className={`nav-link ${isActive("/") ? "active" : ""}`}>
-        <button className="flex items-center space-x-[5px]" onClick={() => handleIconClick("about")}>
-          <h1 className={`hidden sm:inline ${isActive("/") ? "text-blue-500" : ""}`}>ABOUT</h1>
+      <Link to="/" className={`nav-link ${isActive('/') ? 'active' : ''}`}>
+        <button
+          className="flex items-center space-x-[5px]"
+          onClick={() => handleIconClick('about')}
+        >
+          <h1
+            className={`hidden sm:inline ${
+              isActive('/') ? 'text-blue-500' : ''
+            }`}
+          >
+            ABOUT
+          </h1>
         </button>
       </Link>
 
-      <Link to="/people" className={`nav-link ${isActive("/people") ? "active" : ""}`}>
-        <button className="flex items-center space-x-[5px]" onClick={() => handleIconClick("people")}>
-          <FontAwesomeIcon icon={faPeopleGroup} className={`nav-icon ${isActive("/people") ? "text-blue-500" : ""} ${clickedIcon === "people" ? "clicked" : ""}`} />
-          <h1 className={`hidden sm:inline ${isActive("/people") ? "text-blue-500" : ""}`}>TEAM</h1>
+      <Link
+        to="/people"
+        className={`nav-link ${isActive('/people') ? 'active' : ''}`}
+      >
+        <button
+          className="flex items-center space-x-[5px]"
+          onClick={() => handleIconClick('people')}
+        >
+          <FontAwesomeIcon
+            icon={faPeopleGroup}
+            className={`nav-icon ${
+              isActive('/people') ? 'text-blue-500' : ''
+            } ${clickedIcon === 'people' ? 'clicked' : ''}`}
+          />
+          <h1
+            className={`hidden sm:inline ${
+              isActive('/people') ? 'text-blue-500' : ''
+            }`}
+          >
+            TEAM
+          </h1>
         </button>
       </Link>
 
-      <Link to="/research" className={`nav-link ${isActive("/research") ? "active" : ""}`}>
-        <button className="flex items-center space-x-[5px]" onClick={() => handleIconClick("research")}>
-          <FontAwesomeIcon icon={faDigging} className={`nav-icon ${isActive("/research") ? "text-blue-500" : ""} ${clickedIcon === "research" ? "clicked" : ""}`} />
-          <h1 className={`hidden sm:inline ${isActive("/research") ? "text-blue-500" : ""}`}>RESEARCH</h1>
+      <Link
+        to="/research"
+        className={`nav-link ${isActive('/research') ? 'active' : ''}`}
+      >
+        <button
+          className="flex items-center space-x-[5px]"
+          onClick={() => handleIconClick('research')}
+        >
+          <FontAwesomeIcon
+            icon={faDigging}
+            className={`nav-icon ${
+              isActive('/research') ? 'text-blue-500' : ''
+            } ${clickedIcon === 'research' ? 'clicked' : ''}`}
+          />
+          <h1
+            className={`hidden sm:inline ${
+              isActive('/research') ? 'text-blue-500' : ''
+            }`}
+          >
+            RESEARCH
+          </h1>
         </button>
       </Link>
 
-      <Link to="/publications" className={`nav-link ${isActive("/publications") ? "active" : ""}`}>
-        <button className="flex items-center space-x-[5px]" onClick={() => handleIconClick("publications")}>
-          <FontAwesomeIcon icon={faNewspaper} className={`nav-icon ${isActive("/publications") ? "text-blue-500" : ""} ${clickedIcon === "publications" ? "clicked" : ""}`} />
-          <h1 className={`hidden sm:inline ${isActive("/publications") ? "text-blue-500" : ""}`}>PUBLICATIONS</h1>
+      <Link
+        to="/publications"
+        className={`nav-link ${isActive('/publications') ? 'active' : ''}`}
+      >
+        <button
+          className="flex items-center space-x-[5px]"
+          onClick={() => handleIconClick('publications')}
+        >
+          <FontAwesomeIcon
+            icon={faNewspaper}
+            className={`nav-icon ${
+              isActive('/publications') ? 'text-blue-500' : ''
+            } ${clickedIcon === 'publications' ? 'clicked' : ''}`}
+          />
+          <h1
+            className={`hidden sm:inline ${
+              isActive('/publications') ? 'text-blue-500' : ''
+            }`}
+          >
+            PUBLICATIONS
+          </h1>
         </button>
       </Link>
 
-      <Link to="/opportunities" className={`nav-link ${isActive("/opportunities") ? "active" : ""}`}>
-        <button className="flex items-center space-x-[5px]" onClick={() => handleIconClick("opportunities")}>
-          <FontAwesomeIcon icon={faFistRaised} className={`nav-icon ${isActive("/opportunities") ? "text-blue-500" : ""} ${clickedIcon === "opportunities" ? "clicked" : ""}`} />
-          <h1 className={`hidden sm:inline ${isActive("/opportunities") ? "text-blue-500" : ""}`}>OPPORTUNITIES</h1>
+      <Link
+        to="/opportunities"
+        className={`nav-link ${isActive('/opportunities') ? 'active' : ''}`}
+      >
+        <button
+          className="flex items-center space-x-[5px]"
+          onClick={() => handleIconClick('opportunities')}
+        >
+          <FontAwesomeIcon
+            icon={faFistRaised}
+            className={`nav-icon ${
+              isActive('/opportunities') ? 'text-blue-500' : ''
+            } ${clickedIcon === 'opportunities' ? 'clicked' : ''}`}
+          />
+          <h1
+            className={`hidden sm:inline ${
+              isActive('/opportunities') ? 'text-blue-500' : ''
+            }`}
+          >
+            OPPORTUNITIES
+          </h1>
         </button>
       </Link>
     </div>
--- src/views/people.tsx
+++ src/views/people.tsx
@@ -1,65 +1,66 @@
-import { Title } from "components/title";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { Title } from 'components/title';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 export const People = () => {
   const sections = [
     {
-      header: "Faculty",
+      header: 'Faculty',
       content: [
         {
-          name: "Ibrahim Sabek",
-          description: "Assistant Professor, Thomas Lord Department of Computer Science.",
-          image: "/people_photos/ibrahimsabek-photo.jpg", 
-          website: "http://viterbi-web.usc.edu/~sabek/",
+          name: 'Ibrahim Sabek',
+          description:
+            'Assistant Professor, Thomas Lord Department of Computer Science.',
+          image: '/people_photos/ibrahimsabek-photo.jpg',
+          website: 'http://viterbi-web.usc.edu/~sabek/',
         },
       ],
     },
     {
-      header: "PhD Students",
+      header: 'PhD Students',
       content: [
         {
-          name: "Shaolin Xie",
-          description: "Fall 2024 -",
+          name: 'Shaolin Xie',
+          description: 'Fall 2024 -',
           image: null,
           website: null,
         },
         {
-          name: "Hanwen Liu",
-          description: "Fall 2024 -",
-          image: null,
-          website: null,
-        },
-      ],
-    },
-    {
-      header: "Master Students",
-      content: [
-        {
-          name: "Qihan Zhang",
-          description: "Fall 2023 -",
+          name: 'Hanwen Liu',
+          description: 'Fall 2024 -',
           image: null,
           website: null,
         },
       ],
     },
     {
-      header: "Undergraduate Students",
+      header: 'Master Students',
       content: [
         {
-          name: "Nicolas Lee",
-          description: "Fall 2024 -",
+          name: 'Qihan Zhang',
+          description: 'Fall 2023 -',
           image: null,
           website: null,
         },
       ],
     },
     {
-      header: "Alumni",
+      header: 'Undergraduate Students',
       content: [
         {
-          name: "Shashank Giridhara",
-          description: "Now: Amazon Redshift",
+          name: 'Nicolas Lee',
+          description: 'Fall 2024 -',
+          image: null,
+          website: null,
+        },
+      ],
+    },
+    {
+      header: 'Alumni',
+      content: [
+        {
+          name: 'Shashank Giridhara',
+          description: 'Now: Amazon Redshift',
           image: null,
           website: null,
         },
@@ -70,43 +71,60 @@ export const People = () => {
   return (
     <div>
       <Title content="People" className="red-title" />
-      <div className="space-y-[30px]">
+      <div className="space-y-8 ">
         {sections.map((section, index) => (
-          <section key={index} className="space-y-[15px]">
-            <h2 className="font-semibold">{section.header}</h2>
-            <div className="flex flex-wrap gap-[20px]">
+          <section key={index} className="space-y-4">
+            <h2 className="font-semibold text-xl">{section.header}</h2>
+            <div
+              className={`${
+                section.header === 'Faculty' ? 'flex w-full' : 'flex flex-wrap'
+              } gap-4 `}
+            >
               {section.content.map((person, index) => (
                 <div
                   key={index}
-                  className="flex w-[300px] space-x-[15px] items-start"
+                  className={`flex items-start ${
+                    section.header === 'Faculty' ? 'w-full gap-8' : 'w-80'
+                  }`}
                 >
                   <div>
                     {person.image ? (
-                      <img
-                        src={person.image}
-                        alt={person.name}
-                        className="w-[100px] h-[100px] object-cover rounded"
-                      />
+                      <div
+                        className={`flex-shrink-0 ${
+                          section.header === 'Faculty' ? 'w-64' : 'w-48'
+                        }`}
+                      >
+                        <img
+                          src={person.image}
+                          alt={person.name}
+                          className="w-full object-cover rounded-lg" // Removed fixed height to maintain aspect ratio
+                        />
+                      </div>
                     ) : (
-                      <div className="bg-gray-100 w-[90px] h-[90px] pt-[5px] px-[5px] flex rounded">
+                      <div className="bg-gray-100 w-24 h-24 flex items-center justify-center rounded">
                         <FontAwesomeIcon
                           icon={faUser}
-                          className="text-gray-300 w-full h-full"
+                          className="text-gray-300 w-12 h-12"
                         />
                       </div>
                     )}
                   </div>
-                  <div>
+                  <div className="ml-4">
                     {person.website ? (
-                      <h2 className="font-semibold">
-                        <a href={person.website} target="_blank" rel="noopener noreferrer" className="clickable-link">
+                      <h2 className="font-semibold text-lg">
+                        <a
+                          href={person.website}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-red-700 hover:underline"
+                        >
                           {person.name}
                         </a>
                       </h2>
                     ) : (
-                      <h2 className="font-semibold">{person.name}</h2>
+                      <h2 className="font-semibold text-lg">{person.name}</h2>
                     )}
-                    <p>{person.description}</p>
+                    <p className="mt-2 text-gray-700">{person.description}</p>
                   </div>
                 </div>
               ))}
